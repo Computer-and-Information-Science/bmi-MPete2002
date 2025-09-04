88bb8e937414e512e3467914ed6c69ea3d76d098
MPete2002 <peter.marino@student.northampton.edu>
2025-09-04 21:09:39 +0000

    Calculated BMI with C++ and calculated 24.563 as the answer.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
- 
+int main() 
 
+{
+
+const int height = 61; 
+const int weight= 130; 
+
+double kg;
+double meters;
+
+// calculations
+
+kg = weight * 0.453592;
+meters = height * 0.0254;
+
+// calculate BMI
+
+double bmi;
+bmi = kg/(meters * meters);
+
+cout << "The bmi for this person is :" << bmi << endl;
 
 }
